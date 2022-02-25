64c896bee460a6d83db07d4c272b916aefd8cb44
Mohammed Alawi <42051256+MohammedAlawi@users.noreply.github.com>
2022-02-25 23:30:30 +0300

    Update url_path_guess.ps1

--- url_path_guess.ps1
+++ url_path_guess.ps1
@@ -21,15 +21,23 @@ if(![System.IO.File]::Exists($path)){
 
 $paths = Get-Content $path
 
-foreach($path in $paths) {
-  try{
-    $req = Invoke-WebRequest -Uri $url.replace("#PATH_G#", $path) -UseBasicParsing -TimeoutSec $timeout
+try{
+  foreach($path in $paths) {
+    try{
+      $req = Invoke-WebRequest -Uri $url.replace("#PATH_G#", $path) -TimeoutSec $timeout
+      $req.AllowAutoRedirect = $false
+    } catch {
+      $code = $_.Exception.Response.StatusCode.value__
+    }
+
     $code = $req.StatusCode
     $body = $req.Content
-  } catch {
-    $code = $_.Exception.Response.StatusCode.value__
+
+    if(@(200, 403).Contains($code)) {
+      "URL`t:`t$($url.replace("#PATH_G#", $path))`nCode`t:`t$code`nBody`t:`t$($body)`n================================================"| Out-File -FilePath $PSScriptRoot\found.txt -Append
+    }
   }
-  if(@(200, 403).Contains($code)) {
-    "URL`t:`t$($url.replace("#PATH_G#", $path))`nBody`t:`t$($body)`n================================================"| Out-File -FilePath $PSScriptRoot\found.txt -Append
-  }
+
+} catch {
+  $_ | Out-File -FilePath $PSScriptRoot\error_ups.txt -Append
 }
